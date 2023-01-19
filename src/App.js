2fdf56560708c1cb16b62a39913f8d1b48d9a06e
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-01-19 23:07:09 +0100

    add useEffect, useDispatch & fetchTables

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Home from './components/pages/Home/Home';
-import SingleTable from './components/pages/Tables/SingleTable';
+import SingleTable from './components/pages/Tables/Table';
 import NotFound from './components/pages/NotFound/NotFound';
 import Header from './components/views/Header/Header';
 import Footer from './components/views/Footer/Footer';
+import { useDispatch } from "react-redux";
+import { fetchTables } from "./redux/tablesRedux";
+import { useEffect } from "react";
 
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => dispatch(fetchTables()), [dispatch]);
+
   return (
     <main>
       <Container>
