728d88ae4f959b78da421650c65aa64eabf8f6e1
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-01-24 14:56:39 +0100

    install json-server

--- server.mjs
+++ server.mjs
@@ -1,6 +1,5 @@
-/* global require, process */
+import jsonServer from 'json-server';
 
-const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('build/db/app.json');
 const middlewares = jsonServer.defaults({
